c6b595a15fb7ae98798dc730309cf27b793fdb80
midlantica <druharper@gmail.com>
2023-07-05 17:25:44 -0500

    1-3 ok

--- pages/course.vue
+++ pages/course.vue
@@ -1,17 +1,16 @@
 <template>
   <div
-    class="p-12 bg-gray-100 w-full h-full min-h-screen flex flex-col items-center"
+    class="flex flex-col items-center w-full h-full min-h-screen p-12 bg-gray-100"
   >
-    <div class="prose mb-12">
+    <div class="mb-12 prose">
       <h1>
-        <span class="font-medium">
-          Course:
-          <span class="font-bold">Mastering Nuxt 3</span>
+        <span class="font-medium text-purple-600">
+          Course: <span class="font-bold">Mastering Nuxt 3</span>
         </span>
       </h1>
     </div>
 
-    <div class="flex flex-row justify-center flex-grow">
+    <div class="flex flex-row justify-center grow">
       <div
         class="prose mr-4 p-8 bg-white rounded-md min-w-[20ch] flex flex-col"
       >
@@ -19,9 +18,8 @@
         <!-- All the lessons for the course listed here -->
       </div>
 
-      <div class="prose p-12 bg-white rounded-md w-[65ch]">
-        <h2>Lesson</h2>
-        <p>This is a lesson</p>
+      <div class="prose p-12 bg-red-100 rounded-md w-[65ch]">
+        <NuxtPage />
       </div>
     </div>
   </div>
---
+++ pages/course/chapter/[chapterSlug]/lesson/[lessonSlug].vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h2>Lessons</h2>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Error mollitia omnis nam perspiciatis ipsam perferendis, odit odio culpa enim ex animi optio, accusamus quisquam cumque doloremque iure in, qui commodi.</p>
+    <p>Chapter slug: {{ $route.params.chapterSlug }}</p>
+    <p>Chapter slug: {{ $route.params.lessonSlug }}</p>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
