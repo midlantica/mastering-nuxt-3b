b2764bce1fd50bf0a5579ff1a3bcf31a2f7dd2d9
midlantica <druharper@gmail.com>
2023-07-31 15:47:43 -0500

    good up to 4-3

--- pages/index.vue
+++ pages/index.vue
@@ -1,1 +1,12 @@
-<template>Hello, world!</template>
+<template>
+  <div class="prose">
+    <h1>Home page</h1>
+    <NuxtLink class="font-medium underline" :href="firstLesson.path">
+      Go to Next Lesson
+    </NuxtLink>
+  </div>
+</template>
+
+<script setup>
+  const firstLesson = useFirstLesson()
+</script>
---
+++ pages/login.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="max-w-2xl prose w0full h-9">
+    <h1>Log in to {{ title }}</h1>
+    <button class="px-4 py-2 font-bold text-white bg-blue-500 rounded">
+      Log in with Github
+    </button>
+  </div>
+</template>
+
+<script setup>
+  const { title } = useCourse()
+</script>
+
+<style scoped></style>
